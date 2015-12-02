ac81118590fbf3cff55b9cb21a4030e49559bbaf
niaproxy <jrr@mail.ru>
2015-12-02 11:13:05 +0300

    Update config window Added list screen

--- list.php
+++ list.php
@@ -5,13 +5,14 @@
 
 <html>
 <head>
-	<title>РЎРёСЃС‚РµРјР° Р°РІС‚РѕРјР°С‚РёС‡РµСЃРєРѕРіРѕ РѕРїРѕРІРµС‰РµРЅРёСЏ</title>
+	<title>Система автоматического оповещения</title>
 	<meta http-equiv="content-type" content="text/html; charset=windows-1251" />
 	<meta http-equiv="Content-Language" content="RU" />
 	<meta http-equiv="imagetoolbar" content="no" />
 	<meta name="MSSmartTagsPreventParsing" content="true" />
 	<meta name="description" content="LGBlue Free Css Template" />
 	<meta name="keywords" content="free,css,template,business" />
+	<META HTTP-EQUIV="refresh" CONTENT="15">
 	<style type="text/css" media="all">@import "images/style.css";</style>
 	
 </head>
@@ -29,46 +30,51 @@
 
 <br>
 
-	<div align=center>	<h2>Р—Р°РїСѓСЃРє СЃРёСЃС‚РµРјС‹ Р°РІС‚РѕРјР°С‚РёС‡РµСЃРєРѕРіРѕ РѕРїРѕРІРµС‰РµРЅРёСЏ</h2></div><br>
+	<div align=center>	<h2>Статус вызовов в системе автоматического оповещения</h2></div><br>
 		
 
 <?php 
  
- mysql_connect("your.hostaddress.com", "username", "password") or die(mysql_error()); 
- mysql_select_db("Database_Name") or die(mysql_error()); 
- $data = mysql_query("SELECT * FROM friends WHERE pet='Cat'") 
+ mysql_connect("localhost", "root", "vicidialnow") or die(mysql_error());
+	  mysql_select_db("asterisk") or die(mysql_error());
+ $data = mysql_query("select phone_number,first_name,last_local_call_time,status from vicidial_list where list_id = 1000 AND status != 'NEW'") 
  or die(mysql_error()); 
- Print "<table border cellpadding=3>"; 
+ Print "<table border cellpadding=3 style=width:100% algin=center>";
+ Print "<th>Номер:</th><th>Имя:</th><th>Время:</th> <th>Статус:</th> "; 
  while($info = mysql_fetch_array( $data )) 
  { 
  Print "<tr>"; 
- Print "<th>Name:</th> <td>".$info['name'] . "</td> "; 
- Print "<th>Color:</th> <td>".$info['fav_color'] . "</td> "; 
- Print "<th>Food:</th> <td>".$info['fav_food'] . "</td> "; 
- Print "<th>Pet:</th> <td>".$info['pet'] . " </td></tr>"; 
+ Print "<td>".$info['phone_number'] . "</td> "; 
+ Print "<td>".$info['first_name'] . "</td> "; 
+ Print "<td>".$info['last_local_call_time'] . "</td> "; 
+ Print "<td>".$info['status'] . " </td></tr>"; 
  } 
  Print "</table>"; 
  ?> 
-<div class="boxads">РџСЂРѕС‚РѕС‚РёРї СЃРёСЃС‚РµРјС‹ РѕРїРѕРІРµС‰РµРЅРёСЏ.
- Р’РµСЂСЃРёСЏ 0.3<br> <b>РСЃС‚РѕС‡РЅРёРєРё РёРЅС„РѕСЂРјР°С†РёРё: </b><br>&#9679; РЁР°Р±Р»РѕРЅС‹ CSS -<a href="http://www.free-css-templates.com">David Herreman </a> 
-<br><b>РЎСЂРµРґР° СЂР°Р·СЂР°Р±РѕС‚РєРё: </b><br>&#9679; Geany.<br> 
-2015Рі. ,РЎР¦РЎ. <a href="mailto:@utg.gazprom.ru"></a></div>
+<FORM>
+<div align=center><INPUT TYPE="button" onClick="history.go(0)" VALUE="Обновить"></div>
+</FORM>
+<div class="boxads">Прототип системы оповещения.
+ Версия 0.3.1<br> <b>Источники информации: </b><br>&#9679; Шаблоны CSS -<a href="http://www.free-css-templates.com">David Herreman </a> 
+<br><b>Среда разработки: </b><br>&#9679; Geany.<br> 
+2015г. ,СЦС. <a href="mailto:@utg.gazprom.ru"></a></div>
 			</div>
 		<div class="leftmenu">
 		
 			<div class="padding">
 	
-<img src="images/top_logo.jpg" alt="Р“Р°Р·РїСЂРѕРј С‚СЂР°РЅСЃРіР°Р· РЎР°СЂР°С‚РѕРІ"/>
+<img src="images/top_logo.jpg" alt="Газпром трансгаз Саратов"/>
 			<br />
 			<hr />
 
-			<h2>РЎСЃС‹Р»РєРё</h2>
+			<h2>Ссылки</h2>
 			<div class="links">
-			<img src="images/arrow.gif" alt="" /> <a href="http://ts.utg.gazprom.ru/telsprav.aspx" target="_blank">РўРµР»РµС„РѕРЅРЅС‹Р№ СЃРїСЂР°РІРѕС‡РЅРёРє РћРћРћ "Р“Р°Р·РїСЂРѕРј С‚СЂР°РЅСЃРіР°Р· РЎР°СЂР°С‚РѕРІ"</a> <br />
-			<img src="images/arrow.gif" alt="" /> <a href="http://www.utg.gazprom.ru/newUTG/default.aspx" target="_blank">РћС„РёС†РёР°Р»СЊРЅС‹Р№ СЃР°Р№С‚ РћРћРћ "Р“Р°Р·РїСЂРѕРј С‚СЂР°РЅСЃРіР°Р· РЎР°СЂР°С‚РѕРІ"</a> <br />
+			<img src="images/arrow.gif" alt="" /> <a href="http://ts.utg.gazprom.ru/telsprav.aspx" target="_blank">Телефонный справочник ООО "Газпром трансгаз Саратов"</a> <br />
+			<img src="images/arrow.gif" alt="" /> <a href="http://www.utg.gazprom.ru/newUTG/default.aspx" target="_blank">Официальный сайт ООО "Газпром трансгаз Саратов"</a> <br />
 			<br>
 			<img src="images/arrow.gif" alt="" /> <a href="http://10.16.101.132" target="_blank">Autodialme</a> <br />
 			<img src="images/arrow.gif" alt="" /> <a href="http://10.16.167.14" target="_blank">Freepbx</a> <br />
+			<img src="images/arrow.gif" alt="" /> <a href="/alarm.php" target="_blank">Запуск системы</a> <br />
 
 
 
@@ -77,7 +83,7 @@
 		</div>
 	</div>
 	<br />&nbsp;<br />
-	<div id="footer">Copyright &copy; 2015 US | Design: РЎР¦РЎ 
+	<div id="footer">Copyright &copy; 2015 US | Design: СЦС 
 		 
 </div>
 	
@@ -85,3 +91,4 @@
 
 </body>
 </html>
+
