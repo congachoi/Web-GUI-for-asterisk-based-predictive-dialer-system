5c826963cce4d7f3bd68113a30a45fb9d9c6ca1e
niaproxy <jrr@mail.ru>
2015-12-03 21:29:49 +0300

    Добавлено поле выбора кода оповещения. Переделана рассылка писем. Переписан код взаимодействия в базой. Опять. Реализован выбор списка абонентов в list.php и окне запуска.

--- list.php
+++ list.php
@@ -6,7 +6,7 @@
 <html>
 <head>
 	<title>Система автоматического оповещения "Сирена"</title>
-	<meta http-equiv="content-type" content="text/html; charset=windows-1251" />
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<meta http-equiv="Content-Language" content="RU" />
 	<meta http-equiv="imagetoolbar" content="no" />
 	<meta name="MSSmartTagsPreventParsing" content="true" />
@@ -32,13 +32,13 @@
 
 	<div align=center>	<h2>Статус вызовов в системе автоматического оповещения</h2></div><br>
 		
-
+<hr>
 <?php 
+$mysql = mysql_connect("localhost", "root", "vicidialnow") or die(mysql_error());
+mysql_select_db("asterisk") or die(mysql_error());
+ if(isset($_POST['list_code'])) {
  
- mysql_connect("localhost", "root", "vicidialnow") or die(mysql_error());
-	  mysql_select_db("asterisk") or die(mysql_error());
- $data = mysql_query("select phone_number,first_name,last_local_call_time,status from vicidial_list where list_id = 1000 AND status != 'NEW'") 
- or die(mysql_error()); 
+ $data = mysql_query("select phone_number,first_name,last_local_call_time,status from vicidial_list where list_id = '". $_POST['list_code']."' AND status != 'NEW'") or die(mysql_error()); 
  Print "<table border cellpadding=3 style=width:100% algin=center>";
  Print "<th>Номер:</th><th>Имя:</th><th>Время:</th> <th>Статус:</th> "; 
  while($info = mysql_fetch_array( $data )) 
@@ -50,12 +50,29 @@
  Print "<td>".$info['status'] . " </td></tr>"; 
  } 
  Print "</table>"; 
- ?> 
-<FORM>
-<div align=center><INPUT TYPE="button" onClick="history.go(0)" VALUE="Обновить"></div>
-</FORM>
+}
+ ?> <hr>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">	
+	<p>
+<h2>Укажи список абонентов:</h2>
+<select name="list_code"> 
+  <option value="">Выбор...</option>
+ <?php 
+ //Список абонентов
+ $list = mysql_query("select list_id,list_name from vicidial_lists") or die(mysql_error());
+  while($list_data = mysql_fetch_array( $list )) 
+ { 
+  Print "<option value=".$list_data['list_id'].">".$list_data['list_name']."</option>";
+}
+ mysql_close($mysql);
+  ?>
+</select>
+</p>
+<hr>
+<div align=center><input type="submit" name="submit" value="Показать"></div>
+</form>
 <div class="boxads">Прототип системы оповещения.
- Версия 0.3.1<br> <b>Источники информации: </b><br>&#9679; Шаблоны CSS -<a href="http://www.free-css-templates.com">David Herreman </a> 
+ Версия 0.5.2<br> <b>Источники информации: </b><br>&#9679; Шаблоны CSS -<a href="http://www.free-css-templates.com">David Herreman </a> 
 <br><b>Среда разработки: </b><br>&#9679; Geany.<br> 
 2015г. ,СЦС. <a href="mailto:@utg.gazprom.ru"></a></div>
 			</div>
