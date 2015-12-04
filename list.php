113fbbce5811bf6425ce46d4af164c51a7bc903f
niaproxy <jrr@mail.ru>
2015-12-04 16:01:07 +0300

    fix codeset list.php

--- list.php
+++ list.php
@@ -5,7 +5,7 @@
 
 <html>
 <head>
-	<title>Система автоматического оповещения "Сирена"</title>
+	<title>РЎРёСЃС‚РµРјР° Р°РІС‚РѕРјР°С‚РёС‡РµСЃРєРѕРіРѕ РѕРїРѕРІРµС‰РµРЅРёСЏ "РЎРёСЂРµРЅР°"</title>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<meta http-equiv="Content-Language" content="RU" />
 	<meta http-equiv="imagetoolbar" content="no" />
@@ -30,7 +30,7 @@
 
 <br>
 
-	<div align=center>	<h2>Статус вызовов в системе автоматического оповещения</h2></div><br>
+	<div align=center>	<h2>РЎС‚Р°С‚СѓСЃ РІС‹Р·РѕРІРѕРІ РІ СЃРёСЃС‚РµРјРµ Р°РІС‚РѕРјР°С‚РёС‡РµСЃРєРѕРіРѕ РѕРїРѕРІРµС‰РµРЅРёСЏ</h2></div><br>
 		
 <hr>
 <?php 
@@ -40,7 +40,7 @@ mysql_select_db("asterisk") or die(mysql_error());
  
  $data = mysql_query("select phone_number,first_name,last_local_call_time,status from vicidial_list where list_id = '". $_POST['list_code']."' AND status != 'NEW'") or die(mysql_error()); 
  Print "<table border cellpadding=3 style=width:100% algin=center>";
- Print "<th>Номер:</th><th>Имя:</th><th>Время:</th> <th>Статус:</th> "; 
+ Print "<th>РќРѕРјРµСЂ:</th><th>РРјСЏ:</th><th>Р’СЂРµРјСЏ:</th> <th>РЎС‚Р°С‚СѓСЃ:</th> "; 
  while($info = mysql_fetch_array( $data )) 
  { 
  Print "<tr>"; 
@@ -54,11 +54,11 @@ mysql_select_db("asterisk") or die(mysql_error());
  ?> <hr>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">	
 	<p>
-<h2>Укажи список абонентов:</h2>
+<h2>РЈРєР°Р¶Рё СЃРїРёСЃРѕРє Р°Р±РѕРЅРµРЅС‚РѕРІ:</h2>
 <select name="list_code"> 
-  <option value="">Выбор...</option>
+  <option value="">Р’С‹Р±РѕСЂ...</option>
  <?php 
- //Список абонентов
+ //РЎРїРёСЃРѕРє Р°Р±РѕРЅРµРЅС‚РѕРІ
  $list = mysql_query("select list_id,list_name from vicidial_lists") or die(mysql_error());
   while($list_data = mysql_fetch_array( $list )) 
  { 
@@ -69,29 +69,29 @@ mysql_select_db("asterisk") or die(mysql_error());
 </select>
 </p>
 <hr>
-<div align=center><input type="submit" name="submit" value="Показать"></div>
+<div align=center><input type="submit" name="submit" value="РџРѕРєР°Р·Р°С‚СЊ"></div>
 </form>
-<div class="boxads">Прототип системы оповещения.
- Версия 0.5.2<br> <b>Источники информации: </b><br>&#9679; Шаблоны CSS -<a href="http://www.free-css-templates.com">David Herreman </a> 
-<br><b>Среда разработки: </b><br>&#9679; Geany.<br> 
-2015г. ,СЦС. <a href="mailto:@utg.gazprom.ru"></a></div>
+<div class="boxads">РџСЂРѕС‚РѕС‚РёРї СЃРёСЃС‚РµРјС‹ РѕРїРѕРІРµС‰РµРЅРёСЏ.
+ Р’РµСЂСЃРёСЏ 0.5.2<br> <b>РСЃС‚РѕС‡РЅРёРєРё РёРЅС„РѕСЂРјР°С†РёРё: </b><br>&#9679; РЁР°Р±Р»РѕРЅС‹ CSS -<a href="http://www.free-css-templates.com">David Herreman </a> 
+<br><b>РЎСЂРµРґР° СЂР°Р·СЂР°Р±РѕС‚РєРё: </b><br>&#9679; Geany.<br> 
+2015Рі. ,РЎР¦РЎ. <a href="mailto:@utg.gazprom.ru"></a></div>
 			</div>
 		<div class="leftmenu">
 		
 			<div class="padding">
 	
-<img src="images/top_logo.jpg" alt="Газпром трансгаз Саратов"/>
+<img src="images/top_logo.jpg" alt="Р“Р°Р·РїСЂРѕРј С‚СЂР°РЅСЃРіР°Р· РЎР°СЂР°С‚РѕРІ"/>
 			<br />
 			<hr />
 
-			<h2>Ссылки</h2>
+			<h2>РЎСЃС‹Р»РєРё</h2>
 			<div class="links">
-			<img src="images/arrow.gif" alt="" /> <a href="http://ts.utg.gazprom.ru/telsprav.aspx" target="_blank">Телефонный справочник ООО "Газпром трансгаз Саратов"</a> <br />
-			<img src="images/arrow.gif" alt="" /> <a href="http://www.utg.gazprom.ru/newUTG/default.aspx" target="_blank">Официальный сайт ООО "Газпром трансгаз Саратов"</a> <br />
+			<img src="images/arrow.gif" alt="" /> <a href="http://ts.utg.gazprom.ru/telsprav.aspx" target="_blank">РўРµР»РµС„РѕРЅРЅС‹Р№ СЃРїСЂР°РІРѕС‡РЅРёРє РћРћРћ "Р“Р°Р·РїСЂРѕРј С‚СЂР°РЅСЃРіР°Р· РЎР°СЂР°С‚РѕРІ"</a> <br />
+			<img src="images/arrow.gif" alt="" /> <a href="http://www.utg.gazprom.ru/newUTG/default.aspx" target="_blank">РћС„РёС†РёР°Р»СЊРЅС‹Р№ СЃР°Р№С‚ РћРћРћ "Р“Р°Р·РїСЂРѕРј С‚СЂР°РЅСЃРіР°Р· РЎР°СЂР°С‚РѕРІ"</a> <br />
 			<br>
 			<img src="images/arrow.gif" alt="" /> <a href="http://10.16.101.132" target="_blank">Autodialme</a> <br />
 			<img src="images/arrow.gif" alt="" /> <a href="http://10.16.167.14" target="_blank">Freepbx</a> <br />
-			<img src="images/arrow.gif" alt="" /> <a href="/alarm.php" target="_blank">Запуск системы</a> <br />
+			<img src="images/arrow.gif" alt="" /> <a href="/alarm.php" target="_blank">Р—Р°РїСѓСЃРє СЃРёСЃС‚РµРјС‹</a> <br />
 
 
 
@@ -100,7 +100,7 @@ mysql_select_db("asterisk") or die(mysql_error());
 		</div>
 	</div>
 	<br />&nbsp;<br />
-	<div id="footer">Copyright &copy; 2015 US | Design: СЦС 
+	<div id="footer">Copyright &copy; 2015 US | Design: РЎР¦РЎ 
 		 
 </div>
 	
